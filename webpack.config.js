41781280fb8f5481ddaa6dedccd9e4cccbce4be4
gabriela <gabriela@sertori.com.br>
2021-12-16 20:10:43 -0300

    fixing run

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -15,16 +14,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    hot: true,
-  },
   plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin(),  
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ].filter(Boolean),
+  ],
   module: {
     rules: [
       {
@@ -32,11 +26,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            plugins: [
-              isDevelopment && require.resolve('react-refresh/babel')
-            ].filter(Boolean)
-          }
         }
       },
       {
