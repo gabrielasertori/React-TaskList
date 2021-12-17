793e36bec4dbc7eaccedb73466f52b648b319543
gabriela <gabriela@sertori.com.br>
2021-12-16 22:03:05 -0300

    deleting tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+	if (newTaskTitle)
+	{
+		setTasks(prevTask => [...prevTask, {
+			id: parseInt(String(Math.random()*1000)),
+			title: newTaskTitle,
+			isComplete: false
+		}]);
+		setNewTaskTitle('');
+	}
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+	const newArr = [...tasks];
+	let taskComplete = newArr.map(item => {
+		if (item.id == id)
+			item.isComplete = !item.isComplete;
+		return item.isComplete;
+	});
+	console.log(taskComplete);
+	console.log(id);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+	const newTasks = tasks.filter(task => task.id !== id)
+	setTasks(newTasks);
   }
 
   return (
@@ -32,9 +51,9 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
@@ -50,7 +69,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -66,7 +85,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
